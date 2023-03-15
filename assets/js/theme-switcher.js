845dbabc39efe7e59a969d48e249fffd14c7fbfa
Adam Laki <info@adamlaki.com>
2023-03-15 19:05:07 +0100

    Fixes

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -10,8 +10,6 @@
     const theme = e.target.getAttribute('data-action');
     const systemMode = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
 
-    console.log(theme);
-
     localStorage.setItem('preferred-theme', theme);
     document.documentElement.setAttribute('data-theme-mode', theme === 'system' ? systemMode : theme);
     themeSwitcher.setAttribute('data-theme-mode', theme);
