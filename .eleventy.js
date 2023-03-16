8fd4a12bfca534c7cfaaff01f4a0fdec5f53cd57
Adam Laki <info@adamlaki.com>
2023-03-16 18:33:44 +0100

    Start with 11ty

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+module.exports = config => {
+  // Put robots.txt in root
+  config.addPassthroughCopy({ './src/robots.txt': '/robots.txt' });
+
+  // Set directories to pass through to the dist folder
+  config.addPassthroughCopy('./src/img/');
+  config.addPassthroughCopy('./src/css/');
+  config.addPassthroughCopy('./src/font/');
+
+  // Returns post items, sorted by issue order
+  config.addCollection('posts', collection => {
+    return [...collection.getFilteredByGlob('./src/posts/*.md')];
+  });
+
+  return {
+    markdownTemplateEngine: 'njk',
+    dataTemplateEngine: 'njk',
+    htmlTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist'
+    }
+  };
+};
